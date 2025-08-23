8d51ba3ea1ae8a43eddaa050e06b55e95204d794
kavelogics-tech <vork.acount.p171@gmail.com>
2025-08-23 15:50:45 +0500

    added search functionality

--- src/app/components/task-manager/DisplayTask.tsx
+++ src/app/components/task-manager/DisplayTask.tsx
@@ -262,7 +262,7 @@ export default function DisplayTask() {
             )}
             {searchQuery && (
               <span className="px-3 py-1 bg-blue-100 text-blue-700 rounded-full text-sm">
-                Search: "{searchQuery}" 🔍
+                Search: &quot;{searchQuery}&quot;🔍
               </span>
             )}
           </div>
--- src/app/dashboard/task-manager/page.tsx
+++ src/app/dashboard/task-manager/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-import AddTaskModal from "@/app/components/task-manager/AddTaskModal";
 import DisplayTask from "@/app/components/task-manager/DisplayTask";
-import { Plus, Search, SlidersHorizontal } from "lucide-react";
 import React, { useState } from "react";
 
 function TaskManager() {
