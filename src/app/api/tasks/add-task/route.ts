549f35c492a738c724f7a302afe526e28c6625fb
kavelogics-tech <vork.acount.p171@gmail.com>
2025-11-03 18:09:22 +0500

    updated accounts

--- src/app/api/tasks/add-task/route.ts
+++ src/app/api/tasks/add-task/route.ts
@@ -1,4 +1,5 @@
 import dbConnect from "@/app/lib/db";
+import { sendEmail } from "@/app/lib/email";
 import Task from "@/app/models/Task";
 import { NextResponse } from "next/server";
 
@@ -8,9 +9,18 @@ export async function POST(req:Request){
     const body=await req.json();
     console.log('Completed field:', body.completed); 
     console.log("Incoming task data:", body); 
+    const title=await body.title;
+    const patientName=await body.patientName;
+    const email='abdurrehman1722@gmail.com'
 
     try {
         const task=await Task.create(body);
+        const emailSubject = "Task Creation by you!";
+              const emailtext = `Healthrive Task Creation `;
+              const emailHTML = `Dear Dr Chioma,<br>
+                Task <b>${title}</b> is succesfully created by you for patient name <b> ${patientName}</b>!
+                <br>Regards; The Healthrive Team`;
+              await sendEmail(email, emailSubject, emailtext, emailHTML);
         return NextResponse.json({success:true,data:task},{status:201})
     } catch (error:unknown) {
         return NextResponse.json({success:false,error},{status:400})
